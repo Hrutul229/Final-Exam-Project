d03af3e41673460d04ef6f4e3db22cfb966c5981
Hrutul Davadkar [Kabir] <123585778+Hrutul229@users.noreply.github.com>
2023-03-10 07:12:20 +0530

    Add files via upload
    the Hrutul Davadkar.pdf is an ppt file

---
+++ Final exam project/index.js
@@ -0,0 +1,9 @@
+/*=================sticky navbar==============*/
+
+window.onscroll = () => {
+
+
+    let header = document.querySelector('.header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
